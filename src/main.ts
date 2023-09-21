762b62a5d3d6a81b7720f17484016ff64c66312f
macrent <macrent@macrentui-MacBookPro.local>
2023-09-21 17:48:43 +0900

    feat: set apiPrefix

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { RequestMethod } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors();
+
+  const configService = app.get(ConfigService);
+
+  app.setGlobalPrefix(configService.get('app.apiPrefix'), {
+    exclude: [{ path: 'health', method: RequestMethod.GET }],
+  });
+
+  await app.listen(configService.get('app.port'));
 }
 bootstrap();
