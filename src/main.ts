905f511526eb4e309a3b184aa460fb834398fa4f
macrent <macrent@macrentui-MacBookPro.local>
2023-09-22 18:23:27 +0900

    feat(swagger): set default swagger

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ConfigService } from '@nestjs/config';
 import { RequestMethod } from '@nestjs/common';
 import { HttpExceptionFilter } from './infra/middleware/http-exception.filter';
 import { ILoggerService } from './shared/logger/interface/logger-service.interface';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,6 +17,17 @@ async function bootstrap() {
   app.setGlobalPrefix(configService.get('app.apiPrefix'), {
     exclude: [{ path: 'health', method: RequestMethod.GET }],
   });
+
+  if (['dev', 'test'].includes(configService.get('app.nodeEnv'))) {
+    const config = new DocumentBuilder()
+      .setTitle(configService.get('app.name'))
+      .setDescription(`The ${configService.get('app.name')} API description`)
+      .setVersion('1.0')
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api-docs', app, document);
+  }
 
   app.useGlobalFilters(new HttpExceptionFilter(logger));
 
